cef02f59151dc929d6a817cb3de9b6d3612b02fb
wireman27 <shreyas.murali@hotmail.com>
2018-07-24 12:05:14 +0530

    modifying to add weighted air links

--- airport_iata.py
+++ airport_iata.py
@@ -7,8 +7,10 @@ import copy
 import numpy
 import json
 
-file_route = '/Users/user/Documents/Cities/India_Air_Links/airline_routes_schedule.csv'
+file_route = '/Users/user/Documents/Cities/india_air_links_weighted/airline_routes_schedule.csv'
+file_capacity = '/Users/user/Documents/Cities/india_air_links_weighted/aircraft_capacities.csv'
 df = pd.read_csv(file_route)
+capacity_df = pd.read_csv(file_capacity)
 serial = df['Sl. No.'].tolist()
 non_iata_loc = list()
 
@@ -38,42 +40,24 @@ for loc in non_iata_loc:
             entry['arr']=df.iloc[row,5]
             entry['dep']=df.iloc[row,7]
             entry['route']=df.iloc[row,11]
+            entry['aircraft_type'] = df.iloc[row,3]
+            entry['frequency'] = df.iloc[row,4]
             routes.append(entry)
     except Exception as err:
         print(serial[loc], loc, err)
 routes_df = pd.DataFrame(routes)
 
+def trip_capacity(row):
+    if row['frequency']== 'Daily':
+        freq = 7
+    else:
+        freq = len(row['frequency'])
+    capa = capacity_df[capacity_df['aircraft_type']==row['aircraft_type']]['capacity']
+    return (int(capa * freq))
 
-## Temporary for creating lnglatiata database
+routes_df['weekly_capacity'] = routes_df.apply(lambda row: trip_capacity(row),axis=1)
 
-##iatas = list()
-##unique_apt = routes_df['pt_airport'].unique().tolist()
-
-##for apt in unique_apt:
-##    entry = dict()
-##    entry['pt_apt']=apt
-##    entry['lng']='nan'
-##    entry['lat']='nan'
-##    entry['apt']='nan'
-##    try:
-##        airport = wikipedia.page(apt + ' airport')
-##        searchObj = re.search(r'IATA: ([A-Z]{3})',airport.content)
-##        if searchObj:
-##            airport_iata = searchObj.group(1)
-##            entry['apt']=airport_iata
-##        lat_lng = airport.coordinates
-##        airport_lat = float(lat_lng[0])
-##        airport_lng = float(lat_lng[1])
-##        entry['lng']=airport_lng
-##        entry['lat']=airport_lat
-##    except Exception as err:
-##        print(apt, ":", err)
-##    iatas.append(entry)   
-##        
-##iatas_df = pd.DataFrame(iatas)
-##iatas_df.to_csv('/Users/user/Documents/Cities/India_Air_Links/airport_iatas.csv')
-
-lnglatiata_df = pd.read_csv('/Users/user/Documents/Cities/India_Air_Links/lnglatiata.csv')
+lnglatiata_df = pd.read_csv('/Users/user/Documents/Cities/india_air_links_weighted/lnglatiata.csv')
 
 def dupe_check(row):
     if str(row['arr']) == 'nan':
@@ -90,24 +74,34 @@ def dupe_check(row):
     return(air1_iata+"-"+air2_iata+"-"+route)
 
 routes_df['dupe_check']=routes_df.apply(lambda row: dupe_check(row),axis=1)
-routes_df.drop_duplicates(subset = ['dupe_check'],keep='first',inplace=True)
+clean = routes_df.drop_duplicates(subset = ['dupe_check'],keep='first')
+
+def get_capacity(row):
+    unique = row['dupe_check']
+    capa = sum(routes_df[routes_df['dupe_check']==unique]['weekly_capacity'])
+    return(int(capa))
+
+clean['tot_weekly_capacity'] = clean.apply(lambda row: get_capacity(row),axis=1)
 
 f_geojson = list()
 
-for route in routes_df['dupe_check']:
+count = 0
+for route in clean['dupe_check']:
     entry = dict()
     rs = route.split('-')
     entry['air1_iata'] = rs[0]
     entry['air2_iata'] = rs[1]
     entry['route'] = rs[2]
+    entry['twc']= clean.iloc[count,9]
     entry['air1_lat'] = lnglatiata_df[lnglatiata_df['iata_apt']==rs[0]].iloc[0,1]
     entry['air1_lng'] = lnglatiata_df[lnglatiata_df['iata_apt']==rs[0]].iloc[0,2]
     entry['air2_lat'] = lnglatiata_df[lnglatiata_df['iata_apt']==rs[1]].iloc[0,1]
     entry['air2_lng'] = lnglatiata_df[lnglatiata_df['iata_apt']==rs[1]].iloc[0,2]
     f_geojson.append(entry)
+    count = count + 1
 
 f_geojson_df = pd.DataFrame(f_geojson)
-f_geojson_df.to_csv('/Users/user/Documents/Cities/India_Air_Links/for_geojson.csv')
+f_geojson_df.to_csv('/Users/user/Documents/Cities/india_air_links_weighted/for_geojson.csv')
 
 llidf = lnglatiata_df
 fgj = f_geojson_df
@@ -119,31 +113,34 @@ geojson = {
 
 for row in range(0,len(llidf.index.tolist())):
     geojson['features'].append({
-        'type': 'Feature',
-        'properties': {
-            'airport_name':llidf.iloc[row,3]
-        },
-	'geometry': {
-            'type': 'Point',
-	    'coordinates': [float(llidf.iloc[row,2]), float(llidf.iloc[row,1])]
-        }
+         'type': 'Feature',
+         'properties': {
+             'airport_name':llidf.iloc[row,3]
+         },
+ 	'geometry': {
+             'type': 'Point',
+             'coordinates': [float(llidf.iloc[row,2]), float(llidf.iloc[row,1])]
+         }
     })
 
 for row in range(0,len(fgj.index.tolist())):
     geojson['features'].append({
         'type': 'Feature',
-        'properties':{},
-	'geometry': {
+        'properties':{
+            'rcs': str(fgj.iloc[row,6]),
+            'twc': int(fgj.iloc[row,7])
+            },
+ 	'geometry': {
             'type': 'LineString',
-	    'coordinates':
-            [
-                [float(fgj.iloc[row,2]), float(fgj.iloc[row,1])],
-                [float(fgj.iloc[row,5]), float(fgj.iloc[row,4])]                                           
-            ]
-        }
+            'coordinates':
+             [
+                 [float(fgj.iloc[row,2]), float(fgj.iloc[row,1])],
+                 [float(fgj.iloc[row,5]), float(fgj.iloc[row,4])]                                           
+             ]
+         }
     })
 
-with open('/Users/user/Documents/Cities/India_Air_Links/air_links_t2.geojson', 'w') as f:
+with open('/Users/user/Documents/Cities/india_air_links_weighted/air_links_t2.geojson', 'w') as f:
     f.write(json.dumps(geojson))
 
 
